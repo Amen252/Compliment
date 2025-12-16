ba8d1ecf4f45a41963840a94ea9872d5d30282a9
Amen252 <mohamedtech.dev@gmail.com>
2025-12-16 12:32:51 +0300

    chaned

--- app/api/categories.ts
+++
@@ -1,6 +0,0 @@
-import api from "../lib/axios";
-// Fetch all categories from API
-export const getAllCategories = async (): Promise<CategoryType[]> => {
-  const res = await api.get("/categories");
-  return res.data;
-};
